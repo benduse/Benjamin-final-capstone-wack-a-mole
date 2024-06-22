3a92bd4392e7a9d87a70a7a0206a8df8e8565488
benduse <110694311+benduse@users.noreply.github.com>
2024-06-21 23:04:49 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,192 +1,186 @@
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
-// TODO: Add the missing query selectors:
-const score=document.querySelector('#score'); 
-const timerDisplay=document.querySelector('#timer');
+const score = document.querySelector('#score');
+const timerDisplay = document.querySelector('#timer');
 
-const audioHit = new Audio("https://github.com/benduse/Benjamin-final-capstone-wack-a-mole/blob/main/assets/hit.mp3");
-const song = new Audio("https://github.com/benduse/Benjamin-final-capstone-wack-a-mole/blob/main/assets/molesong.mp3");
-
-let time = 0;
-let timer;
-let lastHole = 0;
-let points = 0;
-let difficulty = "hard";
-
-
+const audioHit = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/hit.mp3?raw=true");
+const song = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/molesong.mp3?raw=true");
 function playAudio(audioObject) {
   audioObject.play();
 }
-
 function loopAudio(audioObject) {
   audioObject.loop = true;
   playAudio(audioObject);
 }
-
 function stopAudio(audioObject) {
   audioObject.pause();
 }
-
 function play(){
   playAudio(song);
 }
 
-// Generates a random integer within a range.
+let time = 0;
+let timer;
+let lastHole = -1;
+let points = 0;
+let difficulty = "easy";
+
+let testMode = true;
+
+//Generates a random integer within a range.
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//This sets the game difficulty level
+//  Sets the time delay given a difficulty parameter.
 function setDelay(difficulty) {
-  
-  if(difficulty === 'easy'){
-  return 1500
+  if ( difficulty === 'hard' ) {
+    return randomInteger(600,1200);
   }
-  if (difficulty === 'normal'){
-    return 1000
+  if ( difficulty === 'normal') {
+    return 1000;
   }
-  if (difficulty === 'hard') {
-    return randomInteger (600,1200)
-  }
+  return 1500;  
 }
 
-//Chooses a random hole from a list of holes.
+// Chooses a random hole from a list of holes.
 function chooseHole(holes) {
-  
-const index = randomInteger(0,8);
-const hole= holes[index];
-if(hole === lastHole) {
-  return chooseHole(holes)
-} else {
-  lastHole = hole;
-  return hole
-}
-}
-
-//This code determines if the game should stop or continue depending on cases
-function gameOver() {
-  if(time > 0){
-    timeoutID = showUp()
-    return timeoutID
-  } 
-  
-  else{
-    let gameStopped = stopGame()
-    return gameStopped
+  let max = holes.length-1;
+  if ( lastHole != -1 ) {
+    max--;
   }
+  let nextHole = randomInteger(0,max);
+  if ( lastHole != -1 && ( nextHole >= lastHole )) {
+    nextHole++;
+  }
+  lastHole = nextHole;
+
+  return holes[ nextHole ];
 }
 
 
-// Calls the showAndHide function with a specific delay and a hole.
+// The purpose of this function is simply to determine if the game should
+// continue or stop. The game continues if there is still time `if(time > 0)`.
+function gameOver() {
+  return ( time > 0 ) ? showUp() : stopGame();
+}
+
+// Calls the showAndHide() function with a specific delay and a hole.
 function showUp() {
-  let delay = setDelay(difficulty); 
-  const hole = chooseHole(holes);  
+  let delay = setDelay(difficulty);
+  // delay=2000;
+  const hole = chooseHole(holes);
   return showAndHide(hole, delay);
 }
 
-// The purpose of this function is to show and hide the mole given a delay time and the hole where the mole is hidden.
+//  The purpose of this function is to show and hide the mole given
+// a delay time and the hole where the mole is hidden. 
 
 function showAndHide(hole, delay){
-
+  toggleVisibility(hole);
+  whackable = hole.id.substr(4);
+  whacked = false;
   const timeoutID = setTimeout(() => {
     toggleVisibility(hole);
     gameOver();
-  }, 1500); 
+  }, delay);
   return timeoutID;
 }
 
-// Adds or removes show class defined in Css 
+
+//Adds or removes the 'show' class that is defined in styles.css to 
+//  a given hole. It returns the hole.
 function toggleVisibility(hole){
- 
   hole.classList.toggle('show');
   return hole;
 }
 
 // This function increments the points global variable and updates the scoreboard.
 function updateScore() {
-  
- points++;
- score.textContent = points; 
-
+  points++;
+  score.textContent = points;
   return points;
 }
 
-//  This function clears the score by setting points to zero. 
+//This function clears the score by setting `points = 0`. 
 function clearScore() {
-  // TODO: Write your code here
   points = 0;
   score.textContent = points;
   return points;
 }
 
-//  Updates the control board with the timer if time > 0
+// Updates the control board with the timer if time > 0
 function updateTimer() {
-  if (time > 0){
-    time -=1;
-    timerDisplay.textContent = time;
-  }
+  if ( time > 0 ) time--;
+  timerDisplay.textContent = time;
   return time;
 }
 
-//  Starts the timer using setInterval. For each 1000ms (1 second)
+//Starts the timer using setInterval. For each 1000ms (1 second)
 function startTimer() {
   timer = setInterval(updateTimer, 1000);
   return timer;
 }
 
-// when a player hits the mole this gets called and play sounds and updates the scores
+// This is the event handler that gets called when a player
+ //whacks on a mole
 function whack(event) {
-  console.log("wack!")
-  updateScore();
-  playAudio(audioHit);
+  if 
+  ( testMode ||
+       (( event.target.id.substr(4) === whackable ) && !whacked )) {
+    whacked = true;
+    playAudio(audioHit);
+    updateScore();
+  }
   return points;
 }
 
-//  Adds the 'click' event listeners to the moles. 
+// Adds the 'click' event listeners to the moles.
 function setEventListeners(){
-moles.forEach(
-  mole => mole.addEventListener ('click', whack)
-);
+  for ( let mole of moles ) {
+    mole.addEventListener('click',whack);
+  }
   return moles;
 }
 
-/**
-*
-* This function sets the duration of the game. The time limit, in seconds,
-* that a player has to click on the sprites.
-*
-*/
+//This function sets the duration of the game. The time limit, in seconds
 function setDuration(duration) {
   time = duration;
+  timerDisplay.textContent = time;
   return time;
 }
 
-/**
-*
-* This function is called when the game is stopped. It clears the
-* timer using clearInterval. Returns "game stopped".
-*
-*/
+// This function is called when the game is stopped. It clears the
+//timer using clearInterval. Returns "game stopped".
 function stopGame(){
-  stopAudio(song); 
+  stopAudio(song);
   clearInterval(timer);
   return "game stopped";
 }
 
-//  This is the function that starts the game when the `startButton` is clicked.
+//  This is the function that starts the game when the `startButton`
+//  is clicked.
+let done = false;
 function startGame(){
-  loopAudio(song)
-  setDuration(timer);
-  showUp();
-  startTimer();
-  clearScore();
-  setEventListeners();
-  return "game started";
+  if ( testMode && !done ) {
+    setEventListeners();
+    done = true;
+  }
+  if ( testMode || ( time == 0 )) {
+    clearScore();
+    setDuration(10);
+    startTimer();
+    lastHole = -1;
+    showUp();
+    play();
+    return "game started";
+  }
+  return "game already running"
 }
 
 startButton.addEventListener("click", startGame);
-
+if ( !testMode ) setEventListeners();
 
 // Please do not modify the code below.
 // Used for testing purposes.
